b347acb9c6691d40b544875e7e896df3c834d735
Brandon Him <brh55@drexel.edu>
2017-04-09 14:35:20 -0700

    Add color overlay support

--- components/Hero.js
+++ components/Hero.js
@@ -3,6 +3,15 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, View, Dimensions } from 'react-native';
 
+// a -> b
+const createPositionStyle = (zIndex=1) => ({
+  top: 0,
+  zIndex
+});
+
+// a, b -> b
+const setSource = (required, source) => (required) ? source : { uri: source };
+
 export default class Hero extends Component {
   static propTypes = {
     renderOverlay: React.PropTypes.func,
@@ -13,60 +22,76 @@ export default class Hero extends Component {
   constructor(props) {
     super(props);
 
-    const source = setSource(this.props.requireImage, this.props.source);
     this.state = {
-      source,
+      source: setSource(this.props.requireImage, this.props.source),
       opacity: 0,
-      height: this.props.height || undefined,
+      height: this.props.minHeight || undefined,
       resizeMode: this.props.resizeMode || 'cover'
     };
-  }
+  };
 
   componentWillReceiveProps(nextProps) {
-    const source = setSource(this.props.requireImage, this.props.source);
     if (nextProps.source !== this.props.source) {
       this.setState({
-        source
+        source: setSource(this.props.requireImage, this.props.source)
       });
     }
-  }
+  };
 
   componentDidMount() {
     this.setState({
-        opacity: 1
+      opacity: 1
     });
-  }
+  };
+
+  renderColorOverlay() {
+    let overlayStyles = [{
+      height: this.state.height,
+      width: '100%',
+      backgroundColor: this.props.colorOverlay,
+      opacity: this.props.colorOpacity || .30
+    }, createPositionStyle(1), { position: 'absolute'}];
+
+    return (this.props.colorOverlay) ?
+      <View style={overlayStyles}></View> :
+      null;
+  };
+
+  renderHeroOverlay() {
+    const self = this;
+    const transparentBg = { backgroundColor: 'transparent' };
+    const contentStyles = (this.props.colorOverlay) ?
+      [transparentBg, createPositionStyle(2)] :
+      transparentBg;
+
+    const updateViewHeight = (event) => {
+      const overlayHeight = event.nativeEvent.layout.height;
+      if (self.props.minHeight) return;
+
+      if (overlayHeight !== this.state.height) {
+        self.setState({
+          height: overlayHeight
+        });
+      }
+    };
+
+    return (
+      <View
+        onLayout={(event) => updateViewHeight(event)}
+        style={contentStyles}>
+          {this.props.renderOverlay()}
+      </View>
+    );
+  };
 
   render() {
     return (
       <View style={{opacity: this.state.opacity}}>
         <Image source={this.state.source} resizeMode={this.state.resizeMode} style={{height: this.state.height, width: '100%'}}>
-          <View
-            onLayout={(event) => {
-              const overlayHeight = event.nativeEvent.layout.height;
-              if (!this.props.height) {
-                if (overlayHeight !== this.state.height) {
-                  this.setState({
-                    height: overlayHeight
-                  });
-                }
-              }
-            }}
-            style={styles.hero__overlay}>
-              {this.props.renderOverlay()}
-          </View>
+          {this.renderHeroOverlay()}
+          {this.renderColorOverlay()}
         </Image>
       </View>
     )
-  }
+  };
 }
-
-function setSource (required, source) {
-  return (!required) ? { uri: source } : source;
-}
-
-const styles = StyleSheet.create({
-  hero__overlay: {
-    backgroundColor: 'transparent'
-  }
-});
