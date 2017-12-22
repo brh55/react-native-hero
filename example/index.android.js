7cb09f2a3c827ec182859a20bceba3fa7ee602c8
Brandon Him <brandonhim@live.com>
2017-12-22 08:38:33 -0600

    Addd files for update migration

---
+++ example/index.android.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import HeroExample from './App';
+
+AppRegistry.registerComponent('HeroExample', () => HeroExample);
